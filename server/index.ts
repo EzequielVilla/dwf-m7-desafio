5d864f050e2408f9e7d1e312e547bfb4a18b2782
EzequielVilla <ezequiel.n.villa@gmail.com>
2021-10-27 13:20:20 -0300

    add limit

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,9 @@ const port = process.env.PORT || 3000;
 const path = require("path");
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+    limit: "50mb",
+}));
 app.use(express.static("dist"));
 
 
